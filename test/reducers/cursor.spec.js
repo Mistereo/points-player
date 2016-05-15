ecc4bd796c935ea29a90def64a07cc7cb9e8de5f
Mistereo <mistbox@ya.ru>
2016-05-15 21:40:23 +0500

    Add eslint config to tests

--- test/reducers/cursor.spec.js
+++ test/reducers/cursor.spec.js
@@ -1,13 +1,13 @@
-import expect from 'expect';
+import expect from 'expect'
 
-import cursor, { initialState } from '../../src/reducers/cursor';
+import cursor, { initialState } from '../../src/reducers/cursor'
 import {
   prevMove,
   nextMove,
   firstMove,
   lastMove,
-  selectMove
-} from '../../src/actions';
+  selectMove,
+} from '../../src/actions'
 
 const tree = {
   0: { id: 0, childs: [1, 2] },
@@ -22,7 +22,7 @@ describe('cursor reducer', () => {
   })
 
   it('should handle PREV_MOVE', () => {
-    const action = prevMove();
+    const action = prevMove()
     expect(cursor(0, action, tree)).toEqual(0)
     expect(cursor(1, action, tree)).toEqual(0)
     expect(cursor(2, action, tree)).toEqual(0)
@@ -54,11 +54,11 @@ describe('cursor reducer', () => {
   })
 
   it('should handle SELECT_MOVE', () => {
-    const NON_EXIST_NODE = 123123;
+    const NON_EXIST_NODE = 123123
     expect(cursor(0, selectMove(0), tree)).toEqual(0)
     expect(cursor(0, selectMove(1), tree)).toEqual(1)
     expect(cursor(0, selectMove(2), tree)).toEqual(2)
     expect(cursor(0, selectMove(3), tree)).toEqual(3)
     expect(cursor(0, selectMove(NON_EXIST_NODE), tree)).toEqual(0)
   })
-});
+})
